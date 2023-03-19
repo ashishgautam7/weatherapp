b691ec2ac74bc1ac441741bedcefac62f476d100
Aashish Gautam <ashishgautam7.ag@gmail.com>
2023-03-19 15:14:03 -0400

    Initial Commit

---
+++ lab3 Wether Name/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab3 Wether Name
+//
+//  Created by Aashish Gautam on 2023-03-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
